3c1fb6bc4a1f691cbbf7ac49528e351467a75e50
mmhedihasan <mmehedihasansagar@gmail.com>
2023-03-19 23:15:20 +0600

    Create New Branch Amazing Task Manager, Sum File Create, Edit Pubs Yml, images folder Create

---
+++ lib/ui/screens/splashscrenn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScrenn extends StatefulWidget {
+  const SplashScrenn({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScrenn> createState() => _SplashScrennState();
+}
+
+class _SplashScrennState extends State<SplashScrenn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
